8f3c8f9b35cb3dac0363ee56898a4deb2d0a6c2b
yassin <hyassin509@gmail.com>
2022-04-05 15:11:59 +0200

    render products list

---
+++ Client/src/data.js
@@ -0,0 +1,53 @@
+const data={
+    products:[
+        {
+            name:'Nike slim shirt',
+            slug:'adidas-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality shirt'
+        },
+        {
+            name:'Nike fit shirt',
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality shirt'
+        },
+        {
+            name:'Nike slim pant',
+            slug:'adidas-slim-shirt',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality shirt'
+        },
+        {
+            name:'Nike fit pant',
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality shirt'
+        },
+    ]
+}
+export default data
